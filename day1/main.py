e996c8a4a0b09c069a26323dabfeda97a66ee40b
Sander Korteweg <s.korteweg@zandersgroup.com>
2025-12-01 17:26:53 +0100

    feat: add solution to day 1, puzzle 2

--- day1/main.py
+++ day1/main.py
@@ -12,5 +12,19 @@ def problem1():
     return num_zeros
 
 
+def problem2():
+    num_zero_crosses = 0
+
+    with open("day1/input.txt") as f:
+        dial = 50
+        for line in f:
+            multiplier = -1 if line[0] == "L" else 1
+            num_ticks = int(line[1:])
+            new_dial = dial + multiplier * num_ticks
+            dial = new_dial % 100
+            num_zero_crosses += abs(new_dial // 100)
+    return num_zero_crosses
+
+
 if __name__ == "__main__":
-    print(problem1())
+    print(problem2())
